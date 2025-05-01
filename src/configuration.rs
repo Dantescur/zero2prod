751dd3dd0cbca6099c62eff19b9d53f7c7fa409d
Cesar Daniel <cesardaniel.9611@gmail.com>
2025-05-01 00:53:01 -0400

    improved loggin and secrets management

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,24 +1,42 @@
+use secrecy::{ExposeSecret, SecretBox};
+
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
     pub application_port: u16,
 }
 
-#[derive(serde::Deserialize, Clone)]
+#[derive(serde::Deserialize)]
 pub struct DatabaseSettings {
     pub username: String,
-    pub password: String,
+    pub password: SecretBox<String>,
     pub port: u16,
     pub host: String,
     pub database_name: String,
 }
 
+// impl Clone for DatabaseSettings {
+//     fn clone(&self) -> Self {
+//         DatabaseSettings {
+//             database_name: self.database_name.clone(),
+//             host: self.host.clone(),
+//             port: self.port,
+//             username: self.username.clone(),
+//             password: SecretBox::new(Box::new(self.password.expose_secret().clone())),
+//         }
+//     }
+// }
+
 impl DatabaseSettings {
-    pub fn connection_string(&self) -> String {
-        format!(
+    pub fn connection_string(&self) -> SecretBox<String> {
+        SecretBox::new(Box::new(format!(
             "postgres://{}:{}@{}:{}/{}",
-            self.username, self.password, self.host, self.port, self.database_name
-        )
+            self.username,
+            self.password.expose_secret(),
+            self.host,
+            self.port,
+            self.database_name
+        )))
     }
 }
 
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -9,15 +9,27 @@ pub struct FormData {
     name: String,
 }
 
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields(
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    )
+)]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    log::info!(
-        "Adding '{}' '{}' as a new subscriber",
-        form.email,
-        form.name
-    );
+    match insert_subscriber(&pool, &form).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
 
-    log::info!("Saving new subscriber details to the database");
-    match sqlx::query!(
+#[tracing::instrument(
+    name = "Saving new subscriber detaiils in the database",
+    skip(form, pool)
+)]
+pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+    sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
     VALUES ($1, $2, $3 ,$4)
@@ -27,16 +39,11 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         form.name,
         Utc::now()
     )
-    .execute(pool.get_ref())
+    .execute(pool)
     .await
-    {
-        Ok(_) => {
-            log::info!("New subscriber details has been saved.");
-            HttpResponse::Ok().finish()
-        }
-        Err(e) => {
-            log::error!("Failed to execute query: {:?}", e);
-            HttpResponse::InternalServerError().finish()
-        }
-    }
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+    Ok(())
 }
