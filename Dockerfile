5776884c592d1f3c66de98853935a4e8ffbcbaa0
Cesar Daniel <cesardaniel.9611@gmail.com>
2025-05-01 01:49:52 -0400

    Dockerization

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,31 @@
-FROM rust:1.80.1
+FROM lukemathwalker/cargo-chef:latest-rust-1.80.1 as chef
 WORKDIR /app
 RUN apt-get update && apt-get install ldd clang -y
+
+FROM chef as planner
 COPY . .
-RUN cargo build --release
-ENTRYPOINT [ "./target/release/zero2prod" ]
+RUN cargo chef prepare --recipe-path recipe.json
+
+FROM chef as builder
+COPY --from=planner /app/recipe.json recipe.json
+RUN cargo chef cook --release --recipe-path recipe.json
+
+COPY . .
+ENV SQLX_OFFLINE true
+RUN cargo build --release --bin zero2prod
+
+FROM debian:bookworm-slim as runtime
+
+WORKDIR /app
+RUN apt-get update -y \
+  && apt-get install -y --no-install-recommends openssl ca-certificates \
+  # Clean up
+  && apt-get autoremove -y \
+  && apt-get clean -y \
+  && rm -rf /var/lib/apt/lists/*
+COPY --from=builder /app/target/release/zero2prod zero2prod
+
+COPY configuration configuration
+ENV APP_ENVIROMENT production
+ENTRYPOINT [ "./zero2prod" ]
 
--- src/configuration.rs
+++ src/configuration.rs
@@ -1,12 +1,13 @@
 use secrecy::{ExposeSecret, SecretBox};
+use serde::Deserialize;
 
-#[derive(serde::Deserialize)]
+#[derive(Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16,
+    pub application: ApplicationSettings,
 }
 
-#[derive(serde::Deserialize)]
+#[derive(Deserialize)]
 pub struct DatabaseSettings {
     pub username: String,
     pub password: SecretBox<String>,
@@ -15,17 +16,11 @@ pub struct DatabaseSettings {
     pub database_name: String,
 }
 
-// impl Clone for DatabaseSettings {
-//     fn clone(&self) -> Self {
-//         DatabaseSettings {
-//             database_name: self.database_name.clone(),
-//             host: self.host.clone(),
-//             port: self.port,
-//             username: self.username.clone(),
-//             password: SecretBox::new(Box::new(self.password.expose_secret().clone())),
-//         }
-//     }
-// }
+#[derive(Deserialize)]
+pub struct ApplicationSettings {
+    pub port: u16,
+    pub host: String,
+}
 
 impl DatabaseSettings {
     pub fn connection_string(&self) -> SecretBox<String> {
@@ -41,12 +36,53 @@ impl DatabaseSettings {
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory");
+    let configuration_directory = base_path.join("configuration");
+
+    let enviroment: Environment = std::env::var("APP_ENVIROMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIROMENT");
+
+    let enviroment_filename = format!("{}.yaml", enviroment.as_str());
+
     let settings = config::Config::builder()
-        .add_source(config::File::new(
-            "configuration.yaml",
-            config::FileFormat::Yaml,
+        .add_source(config::File::from(
+            configuration_directory.join("base.yaml"),
+        ))
+        .add_source(config::File::from(
+            configuration_directory.join(enviroment_filename),
         ))
         .build()?;
 
     settings.try_deserialize::<Settings>()
+}
+
+pub enum Environment {
+    Local,
+    Production,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match value.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is not a supported enviroment. \
+                Use either `local` or `production`.",
+                other
+            )),
+        }
+    }
 }
